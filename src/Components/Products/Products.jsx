f5036cf90913e2bb199456de00d79ae62d07b3ff
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-07-26 18:17:33 +0300

    added onSale condition

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -3,12 +3,12 @@ import SingleProduct from '../SingleProduct/SingleProduct'
 
 export default function Products() {
     const [products, setProducts] = useState([
-        { id: 1, name: 'Laptop', price: 1000, quantity: 5 },
-        { id: 2, name: 'Mobile', price: 500, quantity: 10 },
-        { id: 3, name: 'Tablet', price: 300, quantity: 15 },
-        { id: 4, name: 'Monitor', price: 200, quantity: 8 },
-        { id: 5, name: 'Keyboard', price: 50, quantity: 20  },
-        { id: 6, name: 'Mouse', price: 25, quantity: 30  }
+        { id: 1, name: 'Laptop', price: 1000, quantity: 5, onSale: "true"},
+        { id: 2, name: 'Mobile', price: 500, quantity: 10, onSale: "false"},
+        { id: 3, name: 'Tablet', price: 300, quantity: 15, onSale: "true"},
+        { id: 4, name: 'Monitor', price: 200, quantity: 8, onSale: "false"},
+        { id: 5, name: 'Keyboard', price: 50, quantity: 20, onSale: "false"},
+        { id: 6, name: 'Mouse', price: 25, quantity: 30, onSale: "true"}
     ])
     return (
         <div>
--- src/Components/SingleProduct/SingleProduct.jsx
+++ src/Components/SingleProduct/SingleProduct.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 export default function SingleProduct({ productDetails }) {
-    const { id, name, price, quantity } = productDetails;
+    const { id, name, price, quantity, onSale } = productDetails;
     return (
-        <div className='bg-gray-100 p-4 rounded-md shadow-md m-2'>
+        <div className='bg-gray-100 p-4 rounded-md shadow-md m-4'>
             <p>ID: {id}</p>
             <p>Name: {name}</p>
             <p>Price: {price}</p>
             <p>Quantity: {quantity}</p>
+            {/* { onSale=='true' ? <div className='bg-red-600 text-white font-bold rounded-md'>Sale</div> : <div className='bg-green-600 text-white font-bold rounded-md'>Not on Sale</div> } */}
+            { onSale=='true' && <div className='bg-red-600 text-white font-bold rounded-md'>Sale</div>}
         </div>
     )
 }
